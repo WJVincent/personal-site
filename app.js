48ba4f02eeb4e7546126fc11920e9b0faba1df22
William Vincent <william@william-vincent.dev>
2024-05-20 01:06:56 -0400

    refactor and remove rss

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const xmlescape = require('xml-escape');
-const showdown = require('showdown'); // markdown -> html convertor
 const path = require('path');
 
-const { readFile, getBlogPostNames, convertBytes, readRssFeed, getBinTime } = require('./utils.js');
+const { prepareHTML } = require('./utils.js');
 
 const port = process.env.PORT || 5000;
-const index = readFile('html', 'index', 'html');
 
 const app = express();
-const convertor = new showdown.Converter();
 
 app.use('/resume', express.static(path.join(__dirname, 'vincent_william_resume.pdf')));
+
+app.get('/', async (_req, res) => {
+    const home = prepareHTML('', 'home', {fileName: 'home'});
+    res.send(home);
+});
 
 app.get('/42', (_req, res) => {
     res.send(`What is the meaning of life, the universe and everything?  *42*
@@ -20,80 +21,25 @@ app.get('/42', (_req, res) => {
     is...`);
 });
 
-app.get('/', async (_req, res) => {
-    const home = readFile('html', 'home', 'html');
-    const templated =  index.replace(/{%CONTENT%}/g, home);
-    const size = new Blob([templated]).size;
-    const finalRes = templated.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
-    
-    const { hour, minute } = getBinTime();
-    const withClock = finalRes.replace(/{%CLOCK%}/g, `Hour: ${hour}, Minute: ${minute}`)
-    res.send(withClock);
-});
-
-app.get('/blog', async (_req, res) => {
-    const blog = readFile('html', 'blog', 'html');
-    const postData = await getBlogPostNames();
-    const templated =  index.replace(/{%CONTENT%}/g, blog);
-    const templatedWithPostNames = templated.replace(/{%CONTENT%}/g, postData.map(post => post[0]).join(''));
-    const size = new Blob([templatedWithPostNames]).size;
-    const finalRes = templatedWithPostNames.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
-
-    const { hour, minute } = getBinTime();
-    const withClock = finalRes.replace(/{%CLOCK%}/g, `Hour: ${hour}, Minute: ${minute}`)
-    res.send(withClock);
+app.get('/blog', (_req, res) => {
+    const blog = prepareHTML('', 'blog', {fileName: 'blog'});
+    res.send(blog);
 });
 
 app.get('/blog/:name', (req, res) => {
     const {name} = req.params;
-    const blog = readFile('blog_posts', name, 'md');
-    const content = convertor.makeHtml(blog);
-    const templated = index.replace(/{%CONTENT%}/g, `<a href="/blog"><- blog-index</a><div>${content}</div>`);
-    const size = new Blob([templated]).size;
-    const finalRes = templated.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
-
-    const { hour, minute } = getBinTime();
-    const withClock = finalRes.replace(/{%CLOCK%}/g, `Hour: ${hour}, Minute: ${minute}`)
-    res.send(withClock);
+    const blog = prepareHTML('', 'blog_post', {dirName: 'blog_posts', fileName: name, fileType: 'md'});
+    res.send(blog);
 });
 
 app.get('/projects', async (_req, res) => {
-    const projects = readFile('html', 'projects', 'html');
-    const templated =  index.replace(/{%CONTENT%}/g, projects);
-    const size = new Blob([templated]).size;
-    const finalRes = templated.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
-
-    const { hour, minute } = getBinTime();
-    const withClock = finalRes.replace(/{%CLOCK%}/g, `Hour: ${hour}, Minute: ${minute}`)
-    res.send(withClock);
+    const projects = prepareHTML('', 'projects', {fileName: 'projects'});
+    res.send(projects)
 });
 
 app.get('/contact', async (_req, res) => {
-    const contact = readFile('html', 'contact', 'html');
-    const templated =  index.replace(/{%CONTENT%}/g, contact);
-    const size = new Blob([templated]).size;
-    const finalRes = templated.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
-
-    const { hour, minute } = getBinTime();
-    const withClock = finalRes.replace(/{%CLOCK%}/g, `Hour: ${hour}, Minute: ${minute}`)
-    res.send(withClock);
-});
-
-app.get('/feed.rss', async (_req, res) => {
-    let rss = readRssFeed();
-    const blogData = await getBlogPostNames();
-    const blogNames = blogData.map(post => post[1]); 
-
-    blogNames.forEach(fileName => {
-        const [name, _fileExt] = fileName.split('.')
-        const data = readFile('blog_posts', name, 'md');
-        const term = `{%${name}%}`;
-        const re = new RegExp(term);
-        rss = rss.replace(re, xmlescape(convertor.makeHtml(data)));
-    }); 
-
-    res.set('Content-Type', 'text/xml');
-    res.send(rss);
+    const contact = prepareHTML('', 'contact', {fileName: 'contact'});
+    res.send(contact);
 });
 
 app.use((_req, res, _next) => {
--- utils.js
+++ utils.js
@@ -1,61 +1,98 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const showdown = require('showdown'); // markdown -> html convertor
+const convertor = new showdown.Converter();
 
-// code taken from SO, could be broken, test more   
+// code taken from SO, could be broken, test more
 const convertBytes = (x) => {
-  const units = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  const units = [
+    "bytes",
+    "KiB",
+    "MiB",
+    "GiB",
+    "TiB",
+    "PiB",
+    "EiB",
+    "ZiB",
+    "YiB",
+  ];
 
-  let l = 0, n = parseInt(x, 10) || 0;
+  let l = 0,
+    n = parseInt(x, 10) || 0;
 
-  while(n >= 1024 && ++l){
-      n = n/1024;
+  while (n >= 1024 && ++l) {
+    n = n / 1024;
   }
-  
-  return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
-}
 
-const readFile = (dirName, fileName, type) => {
-    return fs.readFileSync(path.join(__dirname, `/${dirName}/${fileName}.${type}`), 'utf-8');
-}
-
-const getBlogPostNames = async () => {
-    const postNames = await fs.promises.readdir('blog_posts');
-
-    const parseFileNames = postNames.map(fileName => {
-        const name = fileName.split('.')[0];
-        const [date, title] = name.split('_');
-        return {
-            data: `<li><a href="/blog/${date}_${title}">${title}</a> -- ${date}</li>`,
-            date,
-            fileName
-        };
-    });
-
-    const sortFilesByDateReverse = parseFileNames.sort((a, b) => {
-        if (a.date < b.date) return 1;
-        if (b.date < a.date) return -1;
-        return 0;
-    });
-
-    const html = sortFilesByDateReverse.map(obj => [obj.data, obj.fileName]);
-
-    return html; 
+  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
 };
 
-const readRssFeed = () => {
-    return fs.readFileSync(path.join(__dirname, '/rss.xml'), 'utf-8');
-}
+const readFile = (dirName, fileName, type) => {
+  return fs.readFileSync(
+    path.join(__dirname, `/${dirName}/${fileName}.${type}`),
+    "utf-8",
+  );
+};
 
-const getBinTime = () => {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
+const getBlogPostNames = () => {
+  const postNames = fs.readdirSync("blog_posts");
 
-    const hoursBin = hours.toString(2);
-    const minutesBin = minutes.toString(2);
+  const parseFileNames = postNames.map((fileName) => {
+    const name = fileName.split(".")[0];
+    const [date, title] = name.split("_");
+    return {
+      data: `<li><a href="/blog/${date}_${title}">${title}</a> -- ${date}</li>`,
+      date,
+      fileName,
+    };
+  });
 
-    return { hour: hoursBin, minute: minutesBin};
-}
+  const sortFilesByDateReverse = parseFileNames.sort((a, b) => {
+    if (a.date < b.date) return 1;
+    if (b.date < a.date) return -1;
+    return 0;
+  });
 
-module.exports = { readFile, getBlogPostNames, convertBytes, readRssFeed, getBinTime };
+  const html = sortFilesByDateReverse.map((obj) => [obj.data, obj.fileName]);
 
+  return html;
+};
+
+const prepareHTML = (prefix, route, readFileOpts) => {
+  if(!readFileOpts.dirName) readFileOpts.dirName = 'html';
+  if(!readFileOpts.fileType) readFileOpts.fileType = 'html';
+
+  const { dirName, fileName, fileType } = readFileOpts;
+  const index = readFile("html", "index", "html");
+  let template = readFile(dirName, fileName, fileType);
+  let withContent;
+
+  if(route === 'blog_post'){
+    const content = convertor.makeHtml(template);
+    withContent = index.replace(/{%CONTENT%}/g, `<a href="/blog"><- blog-index</a><div>${content}</div>`);
+
+  } else {
+    withContent = index.replace(/{%CONTENT%}/g, template);
+  }
+ 
+  let optEdit;
+  if(route === 'blog'){
+    const postData = getBlogPostNames();
+    optEdit = withContent.replace(/{%CONTENT%}/g, postData.map(post=> post[0]).join(''))
+  }
+  
+    
+  if(optEdit) withContent = optEdit;
+
+  const size = new Blob([withContent]).size;
+  const withPageSize = withContent.replace(/{%PAGE_SIZE%}/g, convertBytes(size));
+  const res = withPageSize.replace(/{%ROUTE_PREFIX%}/g, prefix);
+  return res;
+};
+
+module.exports = {
+  readFile,
+  getBlogPostNames,
+  convertBytes,
+  prepareHTML,
+};
